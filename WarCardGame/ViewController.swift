d08e34712da60c5d4ff973bc98778b4439f5066d
Uditha Sampath <udithasampath95@icloud.com>
2021-11-16 11:15:26 +0530

    add project to vis

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var scoreLeft=0
+    var scoreRight=0
+    
+    @IBAction func dealButton(_ sender: UIButton) {
+        let rightNumber=Int.random(in: 2...14)
+        let leftNumber=Int.random(in: 2...14)
+        
+        leftImageView.image=UIImage(named: "card\(leftNumber)")
+        rightImageView.image=UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber>rightNumber{
+            scoreLeft=scoreLeft+1
+        }else if leftNumber<rightNumber{
+            scoreRight=scoreRight+1
+        }else{
+            //Tie
+        }
+        
+        leftScoreLabel.text=String(scoreLeft)
+        rightScoreLabel.text=String(scoreRight)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
